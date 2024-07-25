f6ac934b001cde28168387e01cee577267888d0c
Danillo <danillonc@live.com>
2024-07-25 18:16:52 -0300

    deleting project structure

--- CodeChallenges/CodeChallenges/src/main/java/org/example/validaName/UserNameValidator.java
+++
@@ -1,8 +0,0 @@
-package org.example.validaName;
-
-public class UserNameValidator {
-
-    public static final String regularExpression = "^[a-zA-Z0-9_]+$";
-    //[a-zA-Z0-9_]+
-    //^[a-zA-Z][A-Za-z0-9_]+$
-}
